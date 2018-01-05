62e6e58286d94153ecce0dd5a3aaec53c6f6b2ba
saiful <saiful121968@gmail.com>
2018-01-05 14:42:30 -0500

    File Fat_1 added

---
+++ src/Slim_1/Fat_1.java
@@ -0,0 +1,18 @@
+package Slim_1;
+
+public class Fat_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double Value1, Value2, Sum;
+		Value1 = 3.123456789;
+		Value2 = 3.5E02;
+		Sum = Value1 + Value2;
+		
+		System.out.println("Value1 = " +Value1);
+		System.out.println("Value2 = " + Value2);
+		System.out.println("Sum = " + Sum);
+
+	}
+
+}
